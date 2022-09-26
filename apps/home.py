3efc607433ca74f7554d2444c82c29bf85b6eb03
tsunamifi <104786105+tsunamifi@users.noreply.github.com>
2022-09-26 09:13:07 -0500

    Delete apps directory

--- apps/home.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def app():
-    st.title('Home')
-
-    st.write('Welcome to my software projects portfolio.')
-
-    st.write('Choose which app you would like to interact with')

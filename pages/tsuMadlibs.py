3ec9e99ecd963201b5fd30037a320cb0a56a2bc3
tsunamifi <104786105+tsunamifi@users.noreply.github.com>
2022-09-26 11:06:09 -0500

    move into pages folder
    fixed up some grammar in storylines

--- pages/tsuMadlibs.py
+++ pages/tsuMadlibs.py
@@ -37,32 +37,32 @@ def madlib1():
     
     with ML1:
        with st.form(key= 'ml1v'):
-        noun = st.text_input(' enter a noun: ' )
-        noun2 =  st.text_input(' enter another noun: ' ) 
-        verb =   st.text_input('enter a verb(ing):  ' ) 
-        verb2 =   st.text_input('enter a different verb(ing):  ' )
-        bodys =  st.text_input('enter a body part (plural):  ' ) 
-        emo =  st.text_input('enter an emotion:  ' ) 
-        adj =  st.text_input('enter an adjective:  ' ) 
-        adj2 =  st.text_input('enter a second adjective:  ' )
-        adj3 =  st.text_input('enter a third adjective:  ' )
-        name =  st.text_input('enter a male name:  ' ) 
-        name2 =  st.text_input('enter a female name:  ' )
-        drink = st.text_input('enter a type of drink: ') 
-        city = st.text_input('enter a city: ')
+        noun = st.text_input(' Enter a noun: ' )
+        noun2 =  st.text_input(' Enter 2nd noun: ' ) 
+        verb =   st.text_input('Enter a verb(ing):  ' ) 
+        verb2 =   st.text_input('Enter a 2nd verb(ing):  ' )
+        bodys =  st.text_input('Enter a body part (plural):  ' ) 
+        emo =  st.text_input('Enter an emotion:  ' ) 
+        adj =  st.text_input('Enter an adjective:  ' ) 
+        adj2 =  st.text_input('Enter a 2nd adjective:  ' )
+        adj3 =  st.text_input('Enter a 3rd adjective:  ' )
+        name =  st.text_input('Enter a male name:  ' ) 
+        name2 =  st.text_input('Enter a female name:  ' )
+        drink = st.text_input('Enter a type of drink: ') 
+        city = st.text_input('Enter a city: ')
         submit = st.form_submit_button('Submit')
         
         
     stry = (name + ' looked at the ' + adj3 + ' ' + noun + ' in his hands and felt '  +  emo  +    '. He walked over to the window and reflected on his '  +  adj2   +   ' surroundings. He had always loved ' +
            adj  + ' ' +  city  +    ' with its many mountains. It was a place that encouraged his tendency to feel '  +  emo    +  '. Then he saw something in the distance, or rather someone. It was the figure' 
-         ' of '  +  name2   +   '. '  +  name2 + ' was a gentle saint with flawless ' +   bodys  +    ' and a heart filled with ambition. '  +  name   +   ' was shocked. His friends saw him as a helpful hero. Once, he had even helped a puppy cross the road. But not even a remarkable person who had once helped a puppy cross the road, was ' 
-         'prepared for what '  +  name2   +   ' had in store today. "The warrior exams ended, and all the others are '  +  verb   +   ' with their partners", making ' +   name2   +   ' sad. As ' +   name   +   ' stepped '
-         'outside and '   + name2  +    ' came closer, he could see the shivering glint in her eye. '  +  name2   +   ' looked with the pain on her face. She said, in frustration, "There is no one left for me. ' 
-         'I want a partnership, I want to fight and do good with you." '  +  name +     ' looked back, and still fidgeting with the '  +  adj3 + ' ' + noun   +   ', "'  +  name2   +   ', you really want to be my partner?", he replied. ' +
+         ' of '  +  name2   +   '. '  +  name2 + ' was a gentle saint with flawless ' +   bodys  +    ' and a heart filled with ambition. '  +  name   +   ' was shocked. His friends saw him as a helpful hero. Once, he had even helped a puppy cross the road. However, not even a remarkable person who had once helped a puppy cross the road, was ' 
+         'prepared for what '  +  name2   +   ' had in store today. Even from a short distance away from the door you could hear ' +  name2  + ' crying, sniffling. As ' +   name   +   ' stepped '
+         'outside and '   + name2  +    ' came closer, he could see the shivering glint in their eye. '  +  name2   +   ' looked with the pain on her face. She said, in frustration, "The warrior exams ended, and all the others are '  +  verb   +   ' with their partners", making ' +   name2   +   ' sad.  "There is no one left for me.  
+         'I want to be apart of a duo like everyone else, I want to fight and do good with YOU,'  +  name + ' " ' .   +  name +     ' looked back, and still fidgeting with the '  +  adj3 + ' ' + noun   +   ', "'  +  name2   +   ', you really want to partner up with me?", he replied. ' +
            name   +   ' thought it over, and decided it was better than having to keep '  +   verb2  +    ' all these foes by him self, '  +   name    +   ' could use '  +   name2  +     's flawless '   +  bodys  +     ' and heart of ambition. "Its settled' 
-         ', we are a team now." revealed '  +  name   +   ' with a grin. '  +  name2 +     ' looked happy, her emotions erupting like a noisy '   + noun2    +  '. Then '   + name2    +  ' came inside for a nice drink of '   + drink +
+         ', we are a team now." revealed '  +  name   +   ' with a grin. '  +  name2 +     ' looked happy, Their emotions erupting like a noisy '   + noun2    +  '. Then '   + name2    +  ' came inside for a nice drink of '   + drink +
          
-         ' THE END.')
+         ' The End.')
         
         
     with Cover:
@@ -78,18 +78,18 @@ def  madlib2():
    
     with ML2:
        with st.form(key = 'ml2v'):
-        noun = st.text_input(' enter a noun: ' )
-        cash1 =   st.text_input('enter number:  ' ) 
-        cash2 =   st.text_input('enter another number:  ' )
-        name =  st.text_input('enter a name:  ' ) 
-        emo =  st.text_input('enter an emotion(-ness or -ion):  ' ) 
-        adj =  st.text_input('enter an adjective:  ' ) 
-        adj2 =  st.text_input('enter a second adjective:  ' )
-        gender =  st.text_input('boy or girl:  ' ) 
+        noun = st.text_input(' Enter a noun: ' )
+        cash1 =   st.text_input('Enter a number:  ' ) 
+        cash2 =   st.text_input('Enter 2nd number:  ' )
+        name =  st.text_input('Enter a name:  ' ) 
+        emo =  st.text_input('Enter an emotion ending in "-ness or -ion":  ' ) 
+        adj =  st.text_input('Enter an adjective:  ' ) 
+        adj2 =  st.text_input('Enter a 2nd adjective:  ' )
+        gender =  st.text_input('Boy or Girl:  ' ) 
         submit = st.form_submit_button('Submit')
         
         
-    stry =  (    emo  +    ' was on the rise. I had to buy a new car. How much would it cost? $'  +  cash1   +   ' ?   $'  +  cash2  +    ' ? I had no clue but I knew it would be expensive.'
+    stry =  (    emo  +    ' was on the rise. I had to buy a new car. How much would it cost? $'  +  cash1   +   ',000 ?  $ '  +  cash2  +    ' ? I had no clue but I knew it would be expensive.'
 'I got to the dealership, and the salesman did not seem very knowledgeable about cars. He was wearing a tight-fitting '  +   adj2    +  ' suit that looked like it was about to burst at the seams and his hair looked like he had just been electrocuted. When I asked him which one he recommended, he said that he was really fond of the car with ' +   adj   +   ' wheels. '
 ' He slaps the roof of car with the ' +   adj  +    ' wheels and says "This bad '   + gender  +    ' can fit so many ' +   noun    +  ' in it" '
 'I asked the salesman what his name was and he replied, "' +   name   +   "'. I said, "  +  name   +   ', would you be offended if I called you a terrible car salesman?" '
